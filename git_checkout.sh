34bebe72aa70df4b9d472a0fea1e4f7c30f69e06
Glass_saga <glass.saga@gmail.com>
2012-10-25 17:04:17 +0900

    improve git_checkout

--- git_checkout.sh
+++ git_checkout.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+if [ $# -ne 1 ]; then
+  echo "[ERROR] branch not specified"
+  echo "Usage: ./git_checkout.sh BRANCH"
+  exit 1
+fi
+
 for dir in `find deps/leo_* -maxdepth 0`
 do
   cd $dir
+  echo $dir
   git checkout $1
   cd ../../
 done
