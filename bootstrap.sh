a1d19d24edc5f54dc61204e3afc489498ee5e0aa
Yosuke Hara <yousuke.hara@gmail.com>
2015-10-28 15:06:26 +0900

    Fix the copyright

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 #
 # LeoFS
 #
-# Copyright (c) 2012-2014 Rakuten, Inc.
+# Copyright (c) 2012-2015 Rakuten, Inc.
 #
 # This file is provided to you under the Apache License,
 # Version 2.0 (the "License"); you may not use this file
