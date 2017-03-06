027c36d373739e55025bccc45639846610af1066
Wilson Li <windkithk@gmail.com>
2017-03-06 19:40:51 +0900

    leo_gateway: NFS Readdir entries expire with TTL, leo-project/leofs#612

--- apps/leo_gateway/src/leo_gateway_app.erl
+++ apps/leo_gateway/src/leo_gateway_app.erl
@@ -344,6 +344,12 @@ after_process_1(Pid, Managers) ->
                            init_args = [],
                            state = []},
             ok = application:set_env(nfs_rpc_server, args, [MountdArgs, NFS_D_Args, LockdArgs]),
+            {ok, _} = supervisor:start_child(
+                        leo_gateway_sup, {leo_nfs_readdir_state_ets,
+                                          {leo_nfs_readdir_state_ets, start_link,
+                                           [NFS_Options]},
+                                          permanent, 2000, worker,
+                                          [leo_nfs_readdir_state_ets]}),
             ok = application:ensure_started(nfs_rpc_server);
         _ ->
             void
--- apps/leo_gateway/src/leo_nfs_state_ets.erl
+++ apps/leo_gateway/src/leo_nfs_state_ets.erl
@@ -25,13 +25,11 @@
 
 -export([init/1,
          add_path/1, get_path/1, del_path/1, get_uid_list/0, get_path_list/0,
-         add_readdir_entry/2, get_readdir_entry/1, del_readdir_entry/1,
          add_write_verfier/1, get_write_verfier/0]).
 
 
 -define(LEO_GW_NFS_UID2PATH_ETS_TBL, uid2path_ets_tbl).
 -define(LEO_GW_NFS_PATH2UID_ETS_TBL, path2uid_ets_tbl).
--define(LEO_GW_NFS_READDIR_ENTRY_ETS_TBL, readdir_entry_ets_tbl).
 -define(LEO_GW_NFS_WRITE_VERIFIER_ETS_TBL, write_verifier_ets_tbl).
 -define(LEO_GW_NFS_FILEID_GEN_TBL, fileid_gen_ets_tbl).
 -define(LEO_GW_NFS_FILEID_GEN_KEY, counter).
@@ -44,7 +42,6 @@
 init(_Params) ->
     ets:new(?LEO_GW_NFS_UID2PATH_ETS_TBL, [set, named_table, public]),
     ets:new(?LEO_GW_NFS_PATH2UID_ETS_TBL, [set, named_table, public]),
-    ets:new(?LEO_GW_NFS_READDIR_ENTRY_ETS_TBL, [set, named_table, public]),
     ets:new(?LEO_GW_NFS_WRITE_VERIFIER_ETS_TBL, [set, named_table, public]),
     ets:new(?LEO_GW_NFS_FILEID_GEN_TBL, [set, named_table, public]),
     ets:insert(?LEO_GW_NFS_FILEID_GEN_TBL, {?LEO_GW_NFS_FILEID_GEN_KEY, 0}),
@@ -54,16 +51,17 @@ init(_Params) ->
 %% @doc
 add_path(Path4S3) ->
     Ret = ets:lookup(?LEO_GW_NFS_PATH2UID_ETS_TBL, Path4S3),
-    case Ret of
-        [] ->
-            NewUID = uuid(),
-            ets:insert(?LEO_GW_NFS_UID2PATH_ETS_TBL, {NewUID, Path4S3}),
-            ets:insert(?LEO_GW_NFS_PATH2UID_ETS_TBL, {Path4S3, NewUID}),
-            {ok, NewUID};
-        [{_, UID}|_] ->
-            %% already exists
-            {ok, UID}
-    end.
+    {ok, UID_1} = case Ret of
+                      [] ->
+                          NewUID = uuid(),
+                          ets:insert(?LEO_GW_NFS_UID2PATH_ETS_TBL, {NewUID, Path4S3}),
+                          ets:insert(?LEO_GW_NFS_PATH2UID_ETS_TBL, {Path4S3, NewUID}),
+                          {ok, NewUID};
+                      [{_, UID}|_] ->
+                          %% already exists
+                          {ok, UID}
+                  end,
+    {ok, UID_1}.
 
 
 %% @doc
@@ -100,36 +98,14 @@ get_path_list() ->
 
 
 %% @doc
-add_readdir_entry(CookieVerf, ReadDirEntry) ->
-    ets:insert(?LEO_GW_NFS_READDIR_ENTRY_ETS_TBL,
-               {CookieVerf, ReadDirEntry}).
-
-
-%% @doc
-get_readdir_entry(CookieVerf) ->
-    Ret = ets:lookup(?LEO_GW_NFS_READDIR_ENTRY_ETS_TBL, CookieVerf),
-    case Ret of
-        [] ->
-            not_found;
-        [{_, ReadDirEntry}|_] ->
-            {ok, ReadDirEntry}
-    end.
-
-
-%% @doc
-del_readdir_entry(CookieVerf) ->
-    ets:delete(?LEO_GW_NFS_READDIR_ENTRY_ETS_TBL, CookieVerf).
-
-
-%% @doc
 add_write_verfier(WriteVerf) ->
-    ets:insert(?LEO_GW_NFS_READDIR_ENTRY_ETS_TBL,
+    ets:insert(?LEO_GW_NFS_WRITE_VERIFIER_ETS_TBL,
                {write_verf, WriteVerf}).
 
 
 %% @doc
 get_write_verfier() ->
-    Ret = ets:lookup(?LEO_GW_NFS_READDIR_ENTRY_ETS_TBL, write_verf),
+    Ret = ets:lookup(?LEO_GW_NFS_WRITE_VERIFIER_ETS_TBL, write_verf),
     case Ret of
         [] ->
             not_found;
