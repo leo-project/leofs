32e747b2b8287c6ebaf15d362b015a772ac14c67
Wilson Li <windkithk@gmail.com>
2017-03-08 18:00:07 +0900

    leo_gateway: Unit Test with Metadata

--- apps/leo_gateway/test/leo_nfs_readdir_state_ets_server_tests.erl
+++ apps/leo_gateway/test/leo_nfs_readdir_state_ets_server_tests.erl
@@ -28,6 +28,7 @@
 -include("leo_gateway.hrl").
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("leo_logger/include/leo_logger.hrl").
+-include_lib("leo_object_storage/include/leo_object_storage.hrl").
 
 -define(TEST_COOKIE,    <<1,2,3,4,5,6,7,8>>).
 -define(TEST_COOKIE_2,  <<8,7,6,5,4,3,2,1>>).
@@ -67,7 +68,7 @@ setup_mem_thres() ->
     leo_nfs_readdir_state_ets_server:start_link(
       [{nfsd_readdir_scan_int, 180},
        {nfsd_readdir_entry_ttl, 0},
-       {nfsd_readdir_mem_thres, 320}]),
+       {nfsd_readdir_mem_thres, 16384}]),
     ok.
 
 setup_zero_thres() ->
@@ -81,18 +82,20 @@ teardown(_) ->
     leo_nfs_readdir_state_ets_server:stop().
 
 readdir_entry_and_delete() ->
-    leo_nfs_readdir_state_ets_server:add_readdir_entry(?TEST_COOKIE, dummy),
+    Gen1 = gen_entries(1),
+    leo_nfs_readdir_state_ets_server:add_readdir_entry(?TEST_COOKIE, Gen1),
     Cnt_1 = get_count(),
     ?assertEqual(1, Cnt_1),
     Ret = leo_nfs_readdir_state_ets_server:get_readdir_entry(?TEST_COOKIE),
-    ?assertMatch({ok, _}, Ret),
+    ?assertMatch({ok, Gen1}, Ret),
     leo_nfs_readdir_state_ets_server:del_readdir_entry(?TEST_COOKIE),
     Cnt_2 = get_count(),
     ?assertEqual(0, Cnt_2),
     ok.
 
 readdir_entry_and_clean() ->
-    leo_nfs_readdir_state_ets_server:add_readdir_entry(?TEST_COOKIE, dummy),
+    Gen1 = gen_entries(1),
+    leo_nfs_readdir_state_ets_server:add_readdir_entry(?TEST_COOKIE, Gen1),
     Cnt_1 = get_count(),
     ?assertEqual(1, Cnt_1),
     timer:sleep(5000),
@@ -104,11 +107,12 @@ readdir_entry_and_clean() ->
     ok.
 
 readdir_entry_add_clean() ->
-    leo_nfs_readdir_state_ets_server:add_readdir_entry(?TEST_COOKIE, dummy),
+    Gen1 = gen_entries(1),
+    leo_nfs_readdir_state_ets_server:add_readdir_entry(?TEST_COOKIE, Gen1),
     Cnt_1 = get_count(),
     ?assertEqual(1, Cnt_1),
-    timer:sleep(1000),
-    leo_nfs_readdir_state_ets_server:add_readdir_entry(?TEST_COOKIE_2, dummy),
+    Gen2 = gen_entries(2),
+    leo_nfs_readdir_state_ets_server:add_readdir_entry(?TEST_COOKIE_2, Gen2),
     Cnt_2 = get_count(),
     ?assertEqual(1, Cnt_2),
     leo_nfs_readdir_state_ets_server:del_readdir_entry(?TEST_COOKIE_2),
@@ -117,15 +121,32 @@ readdir_entry_add_clean() ->
     ok.
 
 readdir_entry_mem_thres() ->
-    leo_nfs_readdir_state_ets_server:add_readdir_entry(?TEST_COOKIE, dummy),
+    Gen1 = gen_entries(1),
+    leo_nfs_readdir_state_ets_server:add_readdir_entry(?TEST_COOKIE, Gen1),
     Cnt_1 = get_count(),
     ?assertEqual(1, Cnt_1),
-    leo_nfs_readdir_state_ets_server:add_readdir_entry(?TEST_COOKIE_2, dummy2),
+    %% Use around 30KB in ETS
+    Gen100 = gen_entries(100),
+    leo_nfs_readdir_state_ets_server:add_readdir_entry(?TEST_COOKIE_2, Gen100),
     Cnt_2 = get_count(),
     ?assertEqual(2, Cnt_2),
-    leo_nfs_readdir_state_ets_server:add_readdir_entry(?TEST_COOKIE_3, dummy3),
+    leo_nfs_readdir_state_ets_server:add_readdir_entry(?TEST_COOKIE_3, Gen100),
     Cnt_3 = get_count(),
     ?assertEqual(1, Cnt_3),
     ok.
+
+gen_entries(Cnt) ->
+    NowBin = integer_to_binary(leo_date:now()),
+    gen_entries(Cnt, [], NowBin).
+
+gen_entries(0, Acc, _) ->
+    Acc;
+gen_entries(Cnt, Acc, TS) ->
+    CntBin = integer_to_binary(Cnt),
+    Meta = #?METADATA{
+               key = <<"test/test", TS/binary, CntBin/binary>>,
+               checksum = <<"d41d8cd98f00b204e9800998ecf8427e">>
+              },
+    gen_entries(Cnt-1, [Meta | Acc], TS).
 
 -endif.
