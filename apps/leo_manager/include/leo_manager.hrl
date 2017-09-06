9aeeb2500f159cdfe4b50b28f6ccca0e267ef443
Yosuke Hara <yousuke.hara@gmail.com>
2017-09-06 14:00:51 +0900

    manager: Fixes the multi datacenter replication feature for leo-project/leofs/issues/816

--- apps/leo_manager/include/leo_manager.hrl
+++ apps/leo_manager/include/leo_manager.hrl
@@ -320,6 +320,7 @@
 -define(ERROR_ALREADY_HAS_SAME_CLUSTER, "Already has a same neme of cluster").
 -define(ERROR_COULD_NOT_GET_CLUSTER_INFO,"Could not get cluster info").
 -define(ERROR_OVER_MAX_CLUSTERS, "Over max number of clusters").
+-define(ERROR_CLUSTER_NOT_FOUND, "Cluster not found").
 -define(ERROR_UPDATED_SYSTEM_CONF, "Updated the system configuration").
 -define(ERROR_FAILED_UPDATE_LOG_LEVEL, "Failed to update the log-level").
 -define(ERROR_FAILED_GET_VERSION, "Failed to get the version").
