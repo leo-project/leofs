f4810717029acc6462ac0a2e59e4c77faa8f552f
mocchira <nekotaroh@gmail.com>
2018-08-02 17:06:36 +0900

    leofs: Add files related to ubuntu16.04 release automation

---
+++ Dockerfiles/leofs-package-ubuntu16.04/run_container_for_packaging.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+CONTAINER_NAME=leo-ubuntu16.04-packager
+set -xe
+
+#docker pull leoproject/leofs-package-ubuntu16.04:latest
+docker run --rm --privileged=true --name $CONTAINER_NAME leoproject/leofs-package-ubuntu16.04:latest /sbin/init &
+
+# for safe
+sleep 10
+
+docker exec -it $CONTAINER_NAME ./run_packaging.sh $@
+RET=$?
+docker container stop $CONTAINER_NAME
+return $RET
---
+++ Dockerfiles/leofs-package-ubuntu16.04/run_packaging.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -xe
+
+# $1: The release tag will be set
+# $2: The release-id will be set
+
+. ~/erlang/19.3_systemd/activate
+
+# 1. Do packaging
+sh make_deb.sh $1 yes
+
+# 2. Install the package
+dpkg -i leofs_${1}-1_amd64.deb
+chsh -s /bin/bash leofs
+
+# 3. start LeoFS through systemd
+systemctl start leofs-manager-master
+systemctl start leofs-manager-slave
+systemctl start leofs-storage
+systemctl start leofs-gateway
+
+sleep 5
+
+/usr/local/leofs/current/leofs-adm start
+/usr/local/leofs/current/leofs-adm add-bucket test 05236
+
+# 4. Upload the package as a release asset with release-id passed by a shell argument
+# Refer to https://developer.github.com/v3/repos/releases/#upload-a-release-asset
+# TODO
