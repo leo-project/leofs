e3f80e74ae04a80b76f10679235eb20edaa3ba85
mocchira <nekotaroh@gmail.com>
2018-08-03 10:43:21 +0900

    leofs: Add files related to ubuntu14.04 release automation

---
+++ Dockerfiles/leofs-package-ubuntu14.04/run_packaging.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -xe
+
+# $1: The release tag will be set
+# $2: The release-id will be set
+
+. ~/erlang/19.3/activate
+
+# 1. Do packaging
+sh make_deb.sh $1 no
+
+# 2. Install the package
+dpkg -i leofs_${1}-1_amd64.deb
+chsh -s /bin/bash leofs
+
+# 3. start LeoFS
+/usr/local/leofs/current/leo_manager_0/bin/leo_manager start
+/usr/local/leofs/current/leo_manager_0/bin/leo_manager wait_ready
+
+/usr/local/leofs/current/leo_manager_1/bin/leo_manager start
+/usr/local/leofs/current/leo_manager_1/bin/leo_manager wait_ready
+
+/usr/local/leofs/current/leo_storage/bin/leo_storage start
+/usr/local/leofs/current/leo_gateway/bin/leo_gateway start
+/usr/local/leofs/current/leo_storage/bin/leo_storage wait_ready
+/usr/local/leofs/current/leo_gateway/bin/leo_gateway wait_ready
+
+/usr/local/leofs/current/leofs-adm start
+/usr/local/leofs/current/leofs-adm add-bucket test 05236
+
+# 4. Upload the package as a release asset with release-id passed by a shell argument
+# Refer to https://developer.github.com/v3/repos/releases/#upload-a-release-asset
+# TODO
