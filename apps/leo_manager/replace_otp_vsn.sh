26d908fbba53a3b3b3aeb8bbc16a863ac3525237
Yosuke Hara <yousuke.hara@gmail.com>
2018-09-22 10:56:48 +0900

    [manager] To be able to build leo_manager w/Erlang/OTP 21.0

--- apps/leo_manager/replace_otp_vsn.sh
+++ apps/leo_manager/replace_otp_vsn.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find . -name rebar.config|xargs sed -i 's/require_otp_vsn,\s\+"\(.\+\)"/require_otp_vsn, "18|19|20"/g'
+find . -name rebar.config|xargs sed -i 's/require_otp_vsn,\s\+"\(.\+\)"/require_otp_vsn, "19|20|21"/g'
